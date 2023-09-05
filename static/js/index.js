f3da85834c9364a9af6f673e32f93fb33aa68b4f
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-09-05 19:23:09 +0530

    Back to Top button

--- static/js/index.js
+++ static/js/index.js
@@ -74,3 +74,27 @@ screen.onmouseenter = (event) => {
     iteration += 1 / 3;
   }, 30);
 };
+
+// * Back to Top button
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  // Display the button when the user scrolls down 20px from the top of the document
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
