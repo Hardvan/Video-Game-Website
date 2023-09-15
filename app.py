2aa40ef92aa583f83939b8a1c7a8fc4c2e389ab5
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-09-15 19:51:39 +0530

    Modularise json

--- app.py
+++ app.py
@@ -3,17 +3,6 @@ from flask import Flask, render_template, request
 # Load the gamesData.json file
 import json
 
-with open("./static/json/gamesData.json", "r") as f:
-
-    # Load the gamesData.json file
-    gamesData = json.load(f)
-
-    # Get the games list
-    gamesList = gamesData["games"]
-
-    # Close the file
-    f.close()
-
 
 app = Flask(__name__)
 
@@ -21,7 +10,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    return render_template("index.html", gamesList=gamesList)
+    games_list = []
+
+    with open("./static/json/gamesData.json", "r") as f:
+
+        # Load the gamesData.json file
+        games_data = json.load(f)
+
+        # Get the games list
+        games_list = games_data["games"]
+
+        # Close the file
+        f.close()  # Not necessary (since we are using the with statement) but good practice
+
+    return render_template("index.html", games_list=games_list)
 
 
 if __name__ == "__main__":
